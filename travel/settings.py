4f7f6476fca9d33eac785d4cae292a673f3dcead
salina <jyakhwasalina@gmail.com>
2025-11-09 13:03:47 +0545

    Navigation bar update

--- travel/settings.py
+++ travel/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'main',
 ]
+
+SITE_ID = 1
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
