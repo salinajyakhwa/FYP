ca8e855b0b54f70254f09cc3769edb61233c9034
salina <jyakhwasalina@gmail.com>
2025-12-08 21:30:14 +0545

    Add dashboard updates

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import UserProfile, Vendor, TravelPackage, Booking, Review
+from .models import UserProfile, Vendor, TravelPackage, Booking, Review, PackageImage
+
+class PackageImageInline(admin.TabularInline):
+    model = PackageImage
+    extra = 1
+
+class TravelPackageAdmin(admin.ModelAdmin):
+    inlines = [PackageImageInline]
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Vendor)
-admin.site.register(TravelPackage)
+admin.site.register(TravelPackage, TravelPackageAdmin)
 admin.site.register(Booking)
 admin.site.register(Review)
+admin.site.register(PackageImage)
