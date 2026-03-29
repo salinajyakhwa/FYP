f14a0d084dfc1ca0c5438539228f53c6d6c140c8
salina <jyakhwasalina@gmail.com>
2026-03-29 15:41:52 +0545

    Travel type box added in dashboard

--- main/filters.py
+++ main/filters.py
@@ -4,6 +4,7 @@ from .models import TravelPackage
 class TravelPackageFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains', label='Package Name')
     location = django_filters.CharFilter(lookup_expr='icontains', label='Location')
+    travel_type = django_filters.CharFilter(lookup_expr='icontains', label='Travel Type')
     price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt', label='Price from')
     price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt', label='Price to')
     start_date__gt = django_filters.DateFilter(field_name='start_date', lookup_expr='gt', label='Available from')
@@ -12,4 +13,4 @@ class TravelPackageFilter(django_filters.FilterSet):
     
     class Meta:
         model = TravelPackage
-        fields = ['name', 'location', 'price__gt', 'price__lt', 'start_date__gt', 'start_date__lt']
+        fields = ['name', 'location', 'travel_type', 'price__gt', 'price__lt', 'start_date__gt', 'start_date__lt']
